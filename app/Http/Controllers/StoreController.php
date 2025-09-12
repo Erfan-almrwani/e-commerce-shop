4d7bc836f353f49b58be95c18cafab02a770bdb3
Erfan <eng.erfan.m@gmail.com>
2025-09-12 05:32:54 +0300

    feat: Implement advanced Blade features in products and about pages

--- app/Http/Controllers/StoreController.php
+++ app/Http/Controllers/StoreController.php
@@ -6,5 +6,40 @@ use Illuminate\Http\Request;
 
 class StoreController extends Controller
 {
-    //
+    public function products()
+    {
+        $products = [
+            ['name' => 'Smartphone', 'price' => 599.99, 'on_sale' => true, 'description' => 'High-end smartphone'],
+            ['name' => 'Headphones', 'price' => 129.99, 'on_sale' => false, 'description' => 'Wireless headphones'],
+            ['name' => 'Smart Watch', 'price' => 249.99, 'on_sale' => true, 'description' => 'Smart fitness watch'],
+        ];
+
+        return view('shop.products', compact('products'));
+    }
+
+    public function productDetails()
+    {
+        $product = ['name' => 'Smartphone', 'price' => 599.99, 'on_sale' => true, 'description' => 'High-end smartphone'];
+
+        return view('shop.product-details', compact('product'));
+    }
+
+    public function about()
+    {
+        $title = "About Our Store";
+        $description = "We provide the best products for our customers.";
+        $rawHtml = "<strong>Trusted by thousands of customers worldwide!</strong>";
+
+        return view('shop.about-us', compact('title', 'description', 'rawHtml'));
+    }
+
+    public function cart()
+    {
+        return view('shop.cart');
+    }
+
+    public function contact()
+    {
+        return view('shop.contact');
+    }
 }
--- resources/views/shop/about-us.blade.php
+++ resources/views/shop/about-us.blade.php
@@ -3,15 +3,13 @@
 @section('title', 'About Us')
 
 @section('content')
-<div class="container">
-    <h1>About Our Store</h1>
-    <p>
-        We are dedicated to providing the best products and shopping experience for our customers.
-        Our store has a wide selection of items to fit every need.
-    </p>
-    <p>
-        <strong>Our Mission:</strong> Deliver quality and value at the best prices.
-    </p>
-    <p>&copy; {{ date('Y') }} ShopEasy. All rights reserved.</p>
-</div>
+<h1>{{ $title }}</h1>
+<p>{{ $description }}</p>
+
+<div>{!! $rawHtml !!}</div>
+
+@php
+$author = "ShopEasy Team";
+@endphp
+<p>&copy; {{ date('Y') }} - {{ $author }}</p>
 @endsection
