465939fe91d16c95f780ee772b24c88d07c41736
Erfan <eng.erfan.m@gmail.com>
2025-09-12 06:22:47 +0300

    feat: Add routes for all store pages

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ShopController;
+use App\Http\Controllers\StoreController;
+
 Route::get('/', function () {
     return view('index');
 });
 Route::get('/shop',[ShopController::class, 'index']);
+Route::get('/products', [StoreController::class, 'products']);
+Route::get('/product-details', [StoreController::class, 'productDetails']);
+Route::get('/cart', [StoreController::class, 'cart']);
+Route::get('/about-us', [StoreController::class, 'about']);
+Route::get('/contact', [StoreController::class, 'contact']);
